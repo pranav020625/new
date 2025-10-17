22d3d5dc4cf9afa41ab9a2cff9183584b658f5e0
pranav020625 <chittiboyinasriharipranav@gmail.com>
2025-10-17 19:08:19 +0530

    Add 'NOT NOW' message to main method

--- app.java
+++ app.java
@@ -1,11 +1,13 @@
-public class App {
+public class App {S
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
         System.out.println("It is working.");
         System.out.println("It is working again.");
         System.out.println("With out it");
+        System.out.println("NOT NOW");
     }
 }
+
 
 
 
